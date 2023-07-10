f42d46c50e647b9c407ed2bcf5907d14f1314d30
Palak Garg <palakgarg296.com>
2023-07-10 16:28:23 +0530

    update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -49,7 +49,7 @@ const HomePage = () => {
               {error}❗
               <button
                 className=" mt-2 ml-6 px-5 py-2 rounded-md flex items-center bg-gray-800 text-white hover:bg-gray-700 focus:outline-none "
-                onClick={() => navigate(-1)}
+                onClick={() => navigate("/")}
               >
                 Back to <span className="ml-2 text-4xl text-yellow-400"><BiSearch /></span>
               </button>
@@ -60,7 +60,7 @@ const HomePage = () => {
             <div className="rounded-xl overflow-hidden bg-gray-900 md:shadow-2xl w-full md:w-2/3 lg:w-1/3">
               <button
                 className=" mt-2 ml-2 px-5 py-2 rounded-md flex items-center bg-gray-800 text-white hover:bg-gray-700 focus:outline-none "
-                onClick={() => navigate(-1)}
+                onClick={() => navigate("/")}
               >
                 Back to <span className="ml-2 text-2xl text-yellow-400"><BiSearch /></span>
               </button>
